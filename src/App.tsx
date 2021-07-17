e392af96d90ab64818dfc9965a4992af0255aede
Abrar Faisal <abrarfaisal20@gmail.com>
2021-07-18 01:40:58 +0200

    Rendered products and added Item folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 
 // Components
+import Item from "./Item/Item";
 import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
@@ -28,7 +29,27 @@ const App = () => {
         getProducts
     );
     console.log(data);
-    return <div className="App">Start</div>;
+
+    const getTotalItems = () => null;
+
+    const handleAddToCart = (clickedItem: CartItemType) => null;
+
+    const handleRemoveFromCart = () => null;
+
+    if (isLoading) return <LinearProgress />;
+    if (error) return <div>Something went wrong :(</div>;
+
+    return (
+        <Wrapper>
+            <Grid container spacing={3}>
+                {data?.map((item) => (
+                    <Grid item key={item.id} xs={12} sm={4}>
+                        <Item item={item} handleAddToCart={handleAddToCart} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Wrapper>
+    );
 };
 
 export default App;
